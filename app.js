a65b37f72cdfb95562d3855b52307f6d984fa2d4
Giedrius <messagesbox@outlook.com>
2022-07-17 23:10:16 +0300

    7 week final task submit

--- app.js
+++ app.js
@@ -1,84 +1,78 @@
 /* 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai) */
 
-const a = 3;
-	const b = 5;
-	if (a > b) {
-	    console.log(a);
-	} else if (b > a) {
-	    console.log(b);
-	} else {
-	    console.log(a + ' lygu ' + b);
-	}
+console.log('-----1-----');
 
-console.log('----------');
+let a = 9
+let b = 3
+console.log(a === b ? `Lygus` : a > b ? a : b);
 
 
 /* 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai) */
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+console.log('-----2-----');
 
-console.log('----------');
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
 
 
 /* 3. Naudojant for ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai) */
 
-for (let i = 0; i <= 10; i += 2) {
-    console.log(i);    
-}
+console.log('-----3-----');
 
-console.log('----------');
+for(let i = 0; i <= 10; i = i + 2) {
+    console.log(i);
+}
 
 
 /* 4. Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. 
 Išvesti juos konsolėje. (5 taškai) */
 
-function rand(min, max) {
+console.log('-----4-----');
+
+function random(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min)
-};
-
-for (let i = 0; i < 5; i++) {
-    console.log(rand(1, 10));
-};
-
-console.log('----------');
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+for (let i = 1; i <= 10;i++){
+    console.log(`${i}: ${random(1, 10)}`);
+}
 
 
 /* 5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. 
 Paskutinis atspausdintas skaičius turi būti 5. (7 taškai) */
 
-let x = 0
-	while (x !== 5) {
-	    x = random(1, 10)
-	    console.log(x);
-	}
+console.log('-----5-----');
 
-console.log('----------');
+let x = 0
+while (x !== 5) {
+    x = random(1, 10)
+    console.log(x);
+}
 
 
 /* 6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. 
 Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai) */
 
-let mas = [...Array(rand(20, 30))].map(_ => rand(10, 30));
-	console.log(mas);
+console.log('-----6-----');
 
-console.log('----------');
+let masyvas = [...Array(random(20,30))].map(a => random(10,30)).reduce((a,b) => a > b ? a : b)
+console.log(masyvas);
 
 
 /* 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. 
 Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai) */
 
-let gen = [`A`, `B`, `C`, `D`]
-	let count = []
-	let z = [...Array(100)].map(a => gen[random(0, gen.length - 1)]);
-	for(const i of z.flat()) {
-	count[i] ? count[i] += 1 : count[i] = 1
-	}
-	console.log(count);
+console.log('-----7-----');
 
-console.log('----------');
+let ABCD = [`A`, `B`, `C`, `D`]
+let count = []
+let z = [...Array(100)].map(a => ABCD[random(0, ABCD.length - 1)]);
+for(const i of z.flat()) {
+count[i] ? count[i] += 1 : count[i] = 1
+}
+console.log(count);
 
 
 /* 8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. 
@@ -86,23 +80,18 @@ Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negal
 Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. 
 Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų) */
 
-function suma(a, b) {
-    if (typeof a === 'number'
-        && typeof b === 'number') {
-            return a + b;
-        }
-    if (Array.isArray(a)
-        && Array.isArray(b)) {
-            return a.length + b.length
-        }
-    else {
-        throw console.error('Kintamieji tik skaiciai arba masyvai');
+console.log('-----8-----');
+
+function lygineSuma(a,b) {
+    if (typeof a === `number` && typeof b === `number` && ((a + b) % 2) === 0) {
+        return a + b
+    } else if (typeof a === `object` && typeof b === `object` && ((a.length + b.length) % 2) === 0) {
+        return a.length + b.length
+    } else {
+        return `suma nelygine`
     }
 }
-console.log(suma([1, 5, 3], [2, 2]));
-
-
-console.log('----------');
+console.log(lygineSuma([5, 2, 3], [2, 8, 9]));
 
 
 /* 9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. 
@@ -110,6 +99,33 @@ Turi būti patikrinimas, kad kintamasis yra skaičius.
 Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
 skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų) */
 
+console.log('-----9-----');
+
+function pirminisSkaicius(a) {
+    if (typeof a === 'number') {
+        if (a === 1 || a === 0) {
+            return false
+        }
+        if (a === 2) {
+            return true
+        } else {
+            for (let i = 2; i < a; i++) {
+            if (a % i !== 0) {
+            return true
+                } else if (a === i * i) {
+                return false
+             } else {
+                return false
+             }
+             }
+        }
+    } else {
+        return false
+    }
+}
+console.log(pirminisSkaicius(19));
+
 
 /* 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - 
 "(XXX) XXX-XXXX". (10 taškų) */
+
